4ae8b81b1ef1028c4a2b55a9fbe99c4f2ec5c096
Vasili Bandarenka <darken123@inbox.ru>
2023-01-04 23:09:14 +0300

    умножение двух матриц

--- src/TransposeMatrix.java
+++ src/TransposeMatrix.java
@@ -4,18 +4,21 @@
  * Результат транспонирования матрицы – это перевернутая по главной дагонали версия исходной матрицы.*/
 
 public class TransposeMatrix {
-    public static int[][] multiply(int[][] array) {
-        int[][] result = new int[array.length][array.length];
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[0].length; j++) {
-                result[i][j] = array[j][i];
+    public static int[][] multiply(int[][] matrix) {
+
+        int[][] result = new int[matrix[0].length][matrix.length];
+        for (int i = 0; i < matrix[0].length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                result[i][j] = matrix[j][i];
             }
         }
         return result;
     }
 
     public static void main(String[] args) {
-        int[][] arr2D = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}};
+        //int[][] arr2D = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}};
+        int[][] arr2D =   { {-24853, -643535, 5687654}, {7248, 4213, 32} };
+
         printAll(arr2D);
         arr2D = multiply(arr2D);
         printAll(arr2D);
